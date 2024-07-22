469a5daef4bf35fabad4b755eedd68e66fa514b9
Paulo Henrique da Silva Júnior <132317175+paulaoj@users.noreply.github.com>
2024-07-22 20:43:04 -0300

    1

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-var x = document.createElement('div')
-x.style.width = '300px'
-x.style.height = '300px'
-x.style.border = '1px solid black'
-document.body.append(x)
+var item = document.querySelector('.item-price-container')
+item.innerHTML = '20.50'
